394babb810990441ac59b153e1ba825d96094fca
刘勇 <147149271+liuyongcrypto@users.noreply.github.com>
2024-09-24 11:19:24 +0800

    Create Generating_random_numbers_AES.h

---
+++ step2/LELBC/LELBC/Generating_random_numbers_AES.h
@@ -0,0 +1,63 @@
+#ifndef Generating_random_numbers_AES_H
+#define Generating_random_numbers_AES_H
+#include "iostream"
+#include "fstream"
+#include <time.h>
+#include <xmmintrin.h>
+#include <emmintrin.h>
+#include <pmmintrin.h>
+#include <tmmintrin.h>
+#include <smmintrin.h>
+#include <wmmintrin.h>
+#include <intrin.h>  /* for windows */
+/* #include <x86intrin.h>  for linux */
+/* add -maes options for linux system, i.e., g++ -maes */
+#include <stdlib.h>
+using namespace std;
+
+typedef unsigned long long int uint64;
+#pragma intrinsic( __rdtsc )
+
+__inline volatile unsigned long long read_tsc(void)
+{
+	return __rdtsc();
+}
+
+
+#define RAND(a,b) (((a = 36969 * (a & 65535) + (a >> 16)) << 16) + \
+	(b = 18000 * (b & 65535) + (b >> 16))  )
+
+void block_rndfill(unsigned char* buf, const int len)
+{
+	static unsigned long a[2], mt = 1, count = 4;
+	static unsigned char r[4];
+	int                  i;
+
+	if (mt) { mt = 0; *(unsigned long long*)a = read_tsc(); }
+
+	for (i = 0; i < len; ++i)
+	{
+		if (count == 4)
+		{
+			*(unsigned long*)r = RAND(a[0], a[1]);
+			count = 0;
+		}
+
+		buf[i] = r[count++];
+	}
+}
+
+unsigned long long int GenerateRandomValue()
+{
+	__m128i AESkey[10];
+	__m128i rmm00;
+	block_rndfill((unsigned char*)(AESkey), sizeof(AESkey));
+	block_rndfill((unsigned char*)(&rmm00), sizeof(rmm00));
+	rmm00 = _mm_aesenc_si128(_mm_aesenc_si128(_mm_aesenc_si128(_mm_aesenc_si128(_mm_aesenc_si128(_mm_aesenc_si128(_mm_aesenc_si128(_mm_aesenc_si128(_mm_aesenc_si128(_mm_aesenc_si128(rmm00, AESkey[0]), AESkey[1]), AESkey[2]), AESkey[3]), AESkey[4]), AESkey[5]), AESkey[6]), AESkey[7]), AESkey[8]), AESkey[9]);
+	unsigned long long* randnp = (unsigned long long*)(&rmm00);
+	return *randnp;
+}
+
+#endif // !Generating_random_numbers_AES_H
+
+#pragma once
